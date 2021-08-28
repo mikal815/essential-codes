7d5ca42f7fe2010a979c3f91d862d7c82a1c7031
Mike M <mikal815@users.noreply.github.com>
2021-08-28 13:34:36 -0400

    Generate multiple boxes on pageload

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ import { random } from 'lodash'
 
 
 
-
 const Box = ({ color, ...props }) => {
 
     const position = useMemo(() => {
@@ -18,22 +17,10 @@ const Box = ({ color, ...props }) => {
 
     const mesh = useRef();
 
-
-
-    // // This reference will give us direct access to the mesh
-    // const ref = useRef()
-    // // Set up state for the hovered and active state
-    // const [hovered, setHover] = useState(false)
-    // const [active, setActive] = useState(false)
-    // // Rotate mesh every frame, this is outside of React without overhead
-    // useFrame(() => {
-    //     ref.current.rotation.x = ref.current.rotation.y += 0.01
-    // })
-
     const [boxTexture] = useTexture([stone]);
 
     // Array(150).fill().forEach(Box)
-
+    // [...Array(150)].forEach((_, i) => boxIt(i + 1))
 
 
     return (
@@ -41,20 +28,40 @@ const Box = ({ color, ...props }) => {
             {...props}
             position={position}
             ref={mesh}
-        // ref={ref}
-        // scale={active ? 1.5 : 1}
         // onClick={(e) => setActive(!active)}
         // onPointerOver={(e) => setHover(true)}
         // onPointerOut={(e) => setHover(false)}
         >
             <boxGeometry args={[1, 1, 2.3]} />
-            {/* <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} */}
-            <meshStandardMaterial attach="material" map={boxTexture}
-            />
-
+            <meshStandardMaterial attach="material" map={boxTexture} />
         </mesh >
     )
 }
+
+
+
+
+const Boxes = () => {
+    const NUM = 20;
+    const spheres = new Array(NUM).fill()
+    return (
+        <>
+            {spheres.map((_, i) => {
+                return (
+                    <group>
+                        <Box key={i} color="#fff" />
+                        {/* <ambientLight intensity={0.9} />
+                        <pointLight intensity={1.12} position={[0, 0, 0]} /> */}
+                    </group>
+                )
+            })}
+        </>
+    )
+}
+
+
+
+
 
 
 
@@ -63,15 +70,14 @@ export default function App() {
     // const { scene } = useThree();
 
 
-
     return (
         <Canvas>
             <ambientLight intensity={0.5} />
             <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
-            <pointLight position={[-10, -10, -10]} />
+            <pointLight position={[5, 5, 5]} />
             {/* <Box position={[-1.2, 0, 0]} /> */}
             <Suspense fallback={null}>
-                <Box />
+                <Boxes />
             </Suspense>
         </Canvas >
     )
