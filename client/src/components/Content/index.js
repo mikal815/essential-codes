1581de8f4dd7de4a569f9191e7112579f665e394
Mike M <mikal815@users.noreply.github.com>
2022-06-18 14:11:51 -0400

    Format projects section/tinted background

--- client/src/components/Content/index.js
+++ client/src/components/Content/index.js
@@ -147,109 +147,110 @@ export default function Content() {
             </div>
 
             <div className="portfolio" id="projects-link">
-                <h2 id="portfolio-title">Projects</h2>
-                <div className="portfolio-container">
-                    <div className="project">
-                        <div className="inner-title">SURFER DEPOT</div>
-                        <a href="https://surfer-depot.herokuapp.com/" target="_blank" rel="noopener noreferrer">
-                            <img src={SurferDepot} alt="Surfer Depot" />
-                        </a>
-                        <div className="git-link-container">
-                            <a href="https://github.com/mikal815/surfer-depot" className="git-link">VIEW CODE</a>
+                <div className='wrapper'>
+                    <div id="portfolio-title">Projects</div>
+                    <div className="portfolio-container">
+                        <div className="project">
+                            <div className="inner-title">SURFER DEPOT</div>
+                            <a href="https://surfer-depot.herokuapp.com/" target="_blank" rel="noopener noreferrer">
+                                <img src={SurferDepot} alt="Surfer Depot" />
+                            </a>
+                            <div className="git-link-container">
+                                <a href="https://github.com/mikal815/surfer-depot" className="git-link">VIEW CODE</a>
+                            </div>
+                            <div className="caption">
+                                Full stack e-commerce web app. Front-end tech used:
+                                React, Redux, Material-UI, Bootstrap, HTML5, CSS3.
+                                Back-end tech used: MongoDB, Node.js, and Express.
+                                Uses login system with password hashing. User can browse an inventory of surfboards and filter
+                                results by brand, length, shape,
+                                and price. A logged in user can add or remove items from their cart which persists with
+                                MongoDB.
+                                Admin view allows the administrator to add new products onto
+                                the
+                                site. Users must pass authentication to access the admin area.
+                            </div>
                         </div>
-                        <div className="caption">
-                            Full stack e-commerce web app. Front-end tech used:
-                            React, Redux, Material-UI, Bootstrap, HTML5, CSS3.
-                            Back-end tech used: MongoDB, Node.js, and Express.
-                            Uses login system with password hashing. User can browse an inventory of surfboards and filter
-                            results by brand, length, shape,
-                            and price. A logged in user can add or remove items from their cart which persists with
-                            MongoDB.
-                            Admin view allows the administrator to add new products onto
-                            the
-                            site. Users must pass authentication to access the admin area.
+
+                        <div className="project">
+                            <div className="inner-title">ICARUS TRAVEL</div>
+                            <a href="https://superb-science.surge.sh/" target="_blank" rel="noopener noreferrer">
+                                <img src={Icarus} alt="Icarus" />
+                            </a>
+                            <div className="git-link-container">
+                                <a href="https://github.com/mikal815/icarus" className="git-link">VIEW CODE</a>
+                            </div>
+                            <div className="caption">
+                                Website built with HTML5, CSS, and Bootstrap.  Features scrolling image carousels and various animations.
+                            </div>
+                        </div>
+
+                        <div className="project">
+                            <div className="inner-title">SHAPE SHIFTER</div>
+                            <a href="https://fluffy-zoo.surge.sh/" target="_blank" rel="noopener noreferrer">
+                                <img src={Shapes} alt="Shape Shifter" />
+                            </a>
+                            <div className="git-link-container">
+                                <a href="https://github.com/mikal815/clicky-game" className="git-link">VIEW CODE</a>
+                            </div>
+                            <div className="caption">
+                                Memory game build with React. Goal is to click each shape
+                                without clicking the same shape twice. Images are randomly repositioned
+                                after every click increasing difficulty. 12 shapes in total; once your score
+                                reaches 12 you win. Click a shape twice and score is reset to zero and a new game begins. Top
+                                score indicator helps you gauge your progress.
+                            </div>
+                        </div>
+
+                        <div className="project">
+                            <div className="inner-title" id="bps-title">BOARDWALK PIER STAGE</div>
+                            <a href="https://bps-site.surge.sh/" target="_blank" rel="noopener noreferrer">
+                                <img src={BPS} alt="Boardwalk Pier Stage" />
+                            </a>
+                            <div className="git-link-container">
+                                <a href="https://github.com/mikal815/the-event" className="git-link">VIEW CODE</a>
+                            </div>
+                            <div className="caption">
+                                Featuring React on the frontend as well as a countdown clock which displays the amount of
+                                days, hours, minutes and seconds until the event. Other features include a carousel and a side menu drawer.
+                                After clicking a link the user is vertically scrolled directly to the appropriate content.
+                                Animations give a zoom effect on certain elements. An iframe shows the venue's
+                                location on google maps.
+                            </div>
+                        </div>
+
+                        <div className="project">
+                            <div className="inner-title">OCHS BASEBALL</div>
+                            <a href="https://ochs-baseball.surge.sh/" target="_blank" rel="noopener noreferrer">
+                                <img src={OCHS} alt="OCHS Baseball" />
+                            </a>
+                            <div className="git-link-container">
+                                <a href="https://github.com/mikal815/ochs" className="git-link">VIEW CODE</a>
+                            </div>
+                            <div className="caption">
+                                Featuring React for frontend framework, Twitter plugin and carousel. Layout using CSS Grid.
+                            </div>
+                        </div>
+
+                        <div className="project">
+                            <div className="inner-title">MUSIC TO MY EARS</div>
+                            <a href="https://music-to-my-ears-1.herokuapp.com/" target="_blank" rel="noopener noreferrer">
+                                <img src={MTME} alt="Music to My Ears" />
+                            </a>
+                            <div className="git-link-container">
+                                <a href="https://github.com/mikal815/ear-trainer" className="git-link">VIEW CODE</a>
+                            </div>
+                            <div className="caption">
+                                Ear training app for musicians using intervals.
+                                The app runs a sequence of two notes consecutively
+                                and the user must mimic the same sequence using only their ears as a guide. The key
+                                farthest to the left is the key the user must start on.
+                                Built using React on the frontend and Node.js, Express,
+                                and MongoDB (save score) on the backend. Also features a login system using
+                                Passport.js.
+                            </div>
                         </div>
                     </div>
-
-                    <div className="project">
-                        <div className="inner-title">ICARUS TRAVEL</div>
-                        <a href="https://superb-science.surge.sh/" target="_blank" rel="noopener noreferrer">
-                            <img src={Icarus} alt="Icarus" />
-                        </a>
-                        <div className="git-link-container">
-                            <a href="https://github.com/mikal815/icarus" className="git-link">VIEW CODE</a>
-                        </div>
-                        <div className="caption">
-                            Website built with HTML5, CSS, and Bootstrap.  Features scrolling image carousels and various animations.
-                        </div>
-                    </div>
-
-                    <div className="project">
-                        <div className="inner-title">SHAPE SHIFTER</div>
-                        <a href="https://fluffy-zoo.surge.sh/" target="_blank" rel="noopener noreferrer">
-                            <img src={Shapes} alt="Shape Shifter" />
-                        </a>
-                        <div className="git-link-container">
-                            <a href="https://github.com/mikal815/clicky-game" className="git-link">VIEW CODE</a>
-                        </div>
-                        <div className="caption">
-                            Memory game build with React. Goal is to click each shape
-                            without clicking the same shape twice. Images are randomly repositioned
-                            after every click increasing difficulty. 12 shapes in total; once your score
-                            reaches 12 you win. Click a shape twice and score is reset to zero and a new game begins. Top
-                            score indicator helps you gauge your progress.
-                        </div>
-                    </div>
-
-                    <div className="project">
-                        <div className="inner-title" id="bps-title">BOARDWALK PIER STAGE</div>
-                        <a href="https://bps-site.surge.sh/" target="_blank" rel="noopener noreferrer">
-                            <img src={BPS} alt="Boardwalk Pier Stage" />
-                        </a>
-                        <div className="git-link-container">
-                            <a href="https://github.com/mikal815/the-event" className="git-link">VIEW CODE</a>
-                        </div>
-                        <div className="caption">
-                            Featuring React on the frontend as well as a countdown clock which displays the amount of
-                            days, hours, minutes and seconds until the event. Other features include a carousel and a side menu drawer.
-                            After clicking a link the user is vertically scrolled directly to the appropriate content.
-                            Animations give a zoom effect on certain elements. An iframe shows the venue's
-                            location on google maps.
-                        </div>
-                    </div>
-
-                    <div className="project">
-                        <div className="inner-title">OCHS BASEBALL</div>
-                        <a href="https://ochs-baseball.surge.sh/" target="_blank" rel="noopener noreferrer">
-                            <img src={OCHS} alt="OCHS Baseball" />
-                        </a>
-                        <div className="git-link-container">
-                            <a href="https://github.com/mikal815/ochs" className="git-link">VIEW CODE</a>
-                        </div>
-                        <div className="caption">
-                            Featuring React for frontend framework, Twitter plugin and carousel. Layout using CSS Grid.
-                        </div>
-                    </div>
-
-                    <div className="project">
-                        <div className="inner-title">MUSIC TO MY EARS</div>
-                        <a href="https://music-to-my-ears-1.herokuapp.com/" target="_blank" rel="noopener noreferrer">
-                            <img src={MTME} alt="Music to My Ears" />
-                        </a>
-                        <div className="git-link-container">
-                            <a href="https://github.com/mikal815/ear-trainer" className="git-link">VIEW CODE</a>
-                        </div>
-                        <div className="caption">
-                            Ear training app for musicians using intervals.
-                            The app runs a sequence of two notes consecutively
-                            and the user must mimic the same sequence using only their ears as a guide. The key
-                            farthest to the left is the key the user must start on.
-                            Built using React on the frontend and Node.js, Express,
-                            and MongoDB (save score) on the backend. Also features a login system using
-                            Passport.js.
-                        </div>
-                    </div>
-
                 </div>
             </div>
 
